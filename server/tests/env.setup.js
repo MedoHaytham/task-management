56623053b61a9413c0a98430ce0fb581c258f938
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-07-29 16:45:19 +0300

    test: edit environment configuration setup file for test suite

--- server/tests/env.setup.js
+++ server/tests/env.setup.js
@@ -6,6 +6,7 @@ process.env.REFRESH_TOKEN_EXPIRES_IN = '30d';
 process.env.ACCESS_TOKEN_COOKIE_EXPIRES_IN = '15';
 process.env.REFRESH_TOKEN_COOKIE_EXPIRES_IN = '30';
 
+// this database for testing (not using for project)
 process.env.TEST_DATABASE_URI = 'mongodb+srv://projecettest_db_user:<PASSWORD>@cluster0.lklfwpr.mongodb.net/managetasks?retryWrites=true'.replace(
   '<PASSWORD>',
   'dQQc2cTGQbU5IADk'
